77d193ba6af6e7ec25c4d9e6a56429c30736099d
Roberto Meran <romedu1@hotmail.com>
2019-05-29 00:40:42 +0000

    Add the logout function to the verifyToken method and slightly update UI

--- client/src/App.js
+++ client/src/App.js
@@ -51,6 +51,7 @@ const App = () => {
                setUser(user);
             })
             .catch(error => {
+               logoutUser();
                console.log("Create modal: ", error.message);
             })
       }
