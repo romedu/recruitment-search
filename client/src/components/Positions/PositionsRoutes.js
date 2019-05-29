da61d9f5a723494f3d9fd0b42f8cc2291d7bcfa3
Roberto Meran <romedu1@hotmail.com>
2019-05-29 16:30:35 +0000

    Update the Position routes

--- client/src/components/Positions/PositionsRoutes.js
+++ client/src/components/Positions/PositionsRoutes.js
@@ -1,17 +1,24 @@
-import React, {Fragment} from "react";
+import React, {useContext, Fragment} from "react";
 import {Route, Switch} from "react-router-dom";
 import Positions from "./Positions";
 import PositionPage from "./PositionPage";
 import PositionApplication from "./PositionApplication";
+import CandidatesRoutes from "../Candidates/CandidatesRoutes";
+import UserContext from "../../context/user-context";
 
-const PositionsRoutes = () => (
-   <Fragment>
-      <Switch>
-         <Route path="/positions/:positionId/application" component={PositionApplication} />
-         <Route path="/positions/:positionId" component={PositionPage} />
-         <Route exact path="/positions" component={Positions} />
-      </Switch>
-   </Fragment>
-)
+const PositionsRoutes = () => {
+   const userContext = useContext(UserContext);
+   
+   return (
+      <Fragment>
+         <Switch>
+            <Route exact path="/positions" component={Positions} />
+            <Route exact path="/positions/:positionId" component={PositionPage} />
+            {userContext.id && !userContext.isCompany && <Route path="/positions/:positionId/application" component={PositionApplication} />}
+            {userContext.id && userContext.isCompany && <Route path="/positions/:positionId/candidates" component={CandidatesRoutes} />}
+         </Switch>
+      </Fragment>
+   )
+}
 
 export default PositionsRoutes;
