4207651e34510885bae2abb3e551ee447c79fad0
Roberto Meran <romedu1@hotmail.com>
2019-05-30 17:28:21 +0000

    Update what the findUser function returns

--- server/helpers/user.js
+++ server/helpers/user.js
@@ -1,7 +1,7 @@
 exports.findUser = async (req, res, next) => {
    try {
       const {currentUser} = req.locals,
-            {username, password, ...userData} = currentUser;
+            {username, password, ...userData} = currentUser._doc;
 
       res.status(200).json({user: userData});
    }
