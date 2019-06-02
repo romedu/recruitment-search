96abb2848dd84fa9c68512047738c36dce5307ed
romedu <32285662+romedu@users.noreply.github.com>
2019-06-02 02:17:59 -0400

    Populate the company field in the get employee method

--- server/middlewares/employee.js
+++ server/middlewares/employee.js
@@ -18,13 +18,16 @@ exports.isUserDataValid = async (req, res, next) => {
 
 exports.findEmployee = async (req, res, next) => {
    try { 
-      const foundEmployee = await Employee.findById(req.params.id).populate("userData").exec();
+      const foundEmployee = await Employee.findById(req.params.id).populate("userData").populate("company").exec();
 
       if(!foundEmployee) throw createError(404, "Not Found");
 
       // Remove unnecessary/private userData
-      const {username, password, isCompany, positions, applications, ...userData} = foundEmployee.userData._doc;
-      foundEmployee.userData._doc = userData;
+      const employeeData = extractUserData(foundEmployee.userData._doc),
+            companyData = extractUserData(foundEmployee.company._doc);
+      
+      foundEmployee.userData._doc = employeeData;
+      foundEmployee.company._doc = companyData;
 
       req.locals.currentEmployee = foundEmployee;
       next()
@@ -37,12 +40,17 @@ exports.findEmployee = async (req, res, next) => {
 exports.checkIfEmployer = async (req, res, next) => {
    try { 
       const {currentEmployee} = req.locals;
-      if(req.params.userId != currentEmployee.company) throw createError(403, "Unauthorized");
+      if(req.params.userId != currentEmployee.company._id) throw createError(403, "Unauthorized");
       next()
    }
    catch(error) {
       next(error);
    }
+}
+
+const extractUserData = user => {
+   const {username, password, isCompany, positions, applications, ...userData} = user;
+   return userData;
 }
 
 module.exports = exports;
