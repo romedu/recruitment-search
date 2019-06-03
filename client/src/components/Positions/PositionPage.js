6f3529da1b2c9522657889f5df74d996d6c0d95e
Roberto Meran <romedu1@hotmail.com>
2019-06-03 16:00:37 +0000

    Create and implement the delete position functionality

--- client/src/components/Positions/PositionPage.js
+++ client/src/components/Positions/PositionPage.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect, useContext, Fragment} from "react";
 import {Link} from "react-router-dom";
 import PositionData from "./PositionData";
+import Button from "../UI/Button";
 import UserContext from "../../context/user-context";
+import {getFetchOptions} from "../../utils/fetchUtils";
 
 const PositionPage = props => {
     const userContext = useContext(UserContext),
@@ -22,7 +24,21 @@ const PositionPage = props => {
             .catch(error => {
                 console.log("Error: " + error.message);
             })
-    }, [props.match])
+    }, [props.match]);
+    
+    const deletePositionHandler = () => {
+        const {positionId} = props.match.params,
+              token = localStorage.getItem("token"),
+              fetchOptions = getFetchOptions("DELETE", token);
+              
+        fetch(`/api/positions/${positionId}`, fetchOptions)
+            .then(response => response.json())
+            .then(({error}) => {
+                if(error) throw new Error(error.message);
+                props.history.push("/positions");
+            })
+            .catch(error => console.log("Error: ", error.message))
+    }
     
     if(positionState.currentPosition){
         const {positionId} = props.match.params,
@@ -35,7 +51,14 @@ const PositionPage = props => {
                </h2>    
                <PositionData position={positionState.currentPosition} />
                {state && userContext.id && !userContext.isCompany && <Link to={`/positions/${positionId}/application`}> Apply </Link>}
-               {userContext.id === companyId && <Link to={`/positions/${positionId}/candidates`}> Candidates </Link>}
+               {userContext.id === companyId && <Fragment>
+                    <Link to={`/positions/${positionId}/candidates`}>
+                        Candidates
+                    </Link>
+                    <Button action={deletePositionHandler}>
+                        Delete Position
+                    </Button>
+               </Fragment>}
             </Fragment>
         )
     }
