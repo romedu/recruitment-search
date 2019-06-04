da938bf94a3c3e46846545c4c840a4193dc95b47
romedu <32285662+romedu@users.noreply.github.com>
2019-06-03 22:46:27 -0400

    Update and implement the NewExpertiseModal component

--- client/src/components/Profile/ProfileRoutes.js
+++ client/src/components/Profile/ProfileRoutes.js
@@ -1,13 +1,65 @@
 import React, {Fragment} from "react";
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import ProfilePage from "./ProfilePage";
 import UserProperyModal from "./UserPropertyModal";
+import NewExpertiseModal from "./NewExpertiseModal";
 
-const ProfileRoutes = props => (
-   <Fragment>
-      <Route path="/my-profile" component={ProfilePage} />
-      <Route path="/my-profile/:resourceName/create" component={UserProperyModal} />
-   </Fragment>
-)
+const ProfileRoutes = props => {
+   const trainingProperties = [
+            {
+               name: "description",
+               type: "text"
+            },
+            {
+               name: "level",
+               type: "text"
+            },
+            {
+               name: "institution",
+               type: "string"
+            },
+            {
+               name: "startingDate",
+               type: "date"
+            },
+            {
+               name: "endingDate",
+               type: "date"
+            }
+         ], 
+         experiencesProperties = [
+            {
+               name: "companyName",
+               type: "text"
+            },
+            {
+               name: "position",
+               type: "text"
+            },
+            {
+               name: "salary",
+               type: "number"
+            },
+            {
+               name: "startingDate",
+               type: "date"
+            },
+            {
+               name: "endingDate",
+               type: "date"
+            }
+         ];
+
+   return (
+      <Fragment>
+         <Route path="/my-profile" component={ProfilePage} />
+         <Switch>
+            <Route path="/my-profile/trainings/create" render={() => <NewExpertiseModal expertiseName="trainings" properties={trainingProperties} />} />
+            <Route path="/my-profile/workingExperiences/create" render={() => <NewExpertiseModal expertiseName="workingExperiences" properties={experiencesProperties} />} />
+            <Route path="/my-profile/:resourceName/create" component={UserProperyModal} />
+         </Switch>
+      </Fragment>
+   )
+}
 
 export default ProfileRoutes;
